a7f87e5920b4731a404c9dda08437a7869e2bc9b
clarinda123 <144284548+clarinda123@users.noreply.github.com>
2024-01-20 18:40:03 +0000

    Update ec2.tf

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -25,39 +25,39 @@ variable "ami_id" {
   default = "ami-01acac09adf473073" //in oregon US-WEST-2 region
 }
 
-#resource "aws_security_group" "ec2_sg" {
- # name        = join("", [var.name, "-", "ec2-sg"])
-  #description = "Allow  traffic for http and ssh"
+resource "aws_security_group" "ec2_sg" {
+  name        = join("", [var.name, "-", "ec2-sg"])
+  description = "Allow  traffic for http and ssh"
 
 
-  #ingress {
-   # from_port   = 22
-    #to_port     = 22
-    #protocol    = "tcp"
-    #cidr_blocks = ["0.0.0.0/0"]
-  #}
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  #ingress {
-   # from_port   = 9100
-    #to_port     = 9100
-    #protocol    = "tcp"
-    #cidr_blocks = ["0.0.0.0/0"]
-  #}
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  #ingress {
-   # from_port   = 8080
-    #to_port     = 8080
-    #protocol    = "tcp"
-    #cidr_blocks = ["0.0.0.0/0"]
-  #}
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  #egress {
-   # from_port   = 0
-    #to_port     = 0
-    #protocol    = "-1"
-    #cidr_blocks = ["0.0.0.0/0"]
-  #}
-#}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 resource "aws_iam_instance_profile" "instance_profile" {
   name = join("", [var.name, "-", "iam-instance-profile"])
