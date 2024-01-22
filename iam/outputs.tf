32d189786c9f513339bd6694b45e0cdb862687e4
clarinda123 <144284548+clarinda123@users.noreply.github.com>
2024-01-22 03:49:37 +0000

    Update outputs.tf

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_iam_role" {
-  value = aws_iam_role.iam_role1.arn
+  value = aws_iam_role.iam_role.arn
 }
 
 output "ec2_iam_role_name" {
-  value = aws_iam_role.iam_role1.name
+  value = aws_iam_role.iam_role.name
 }
