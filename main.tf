a54c44d81c83abbf4a1ef868cc9a9b465a30b9da
clarinda123 <144284548+clarinda123@users.noreply.github.com>
2024-01-22 03:57:22 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "ec2_dev" {
   source = "./ec2"
   name   = "dev"
   tags          = local.common_tags
-  iam_role_name = module.iam.ec2_iam_role1_name
+  iam_role_name = module.iam.ec2_iam_role_name
   key_pair_name = var.key_pair_name
 }
 
@@ -21,7 +21,7 @@ module "ec2_stage" {
   source = "./ec2"
   name   = "stage"
   tags          = local.common_tags
-  iam_role_name = module.iam.ec2_iam_role1_name
+  iam_role_name = module.iam.ec2_iam_role_name
   key_pair_name = var.key_pair_name
 }
 
@@ -30,6 +30,6 @@ module "ec2_prod" {
   source = "./ec2"
   name   = "prod"
   tags          = local.common_tags
-  iam_role_name = module.iam.ec2_iam_role1_name
+  iam_role_name = module.iam.ec2_iam_role_name
   key_pair_name = var.key_pair_name
 }
